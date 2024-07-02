fef8fef9390f387c10f854cfd940c8e5a959a3d0
AlperenI <alperenuniv@gmail.com>
2024-07-02 13:58:15 +0300

    json eklendi

--- lib/firebase/register_page.dart
+++ lib/firebase/register_page.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -10,8 +11,27 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  final _emailcontroller=TextEditingController();
-  final _passwordcontroller=TextEditingController();
+  final _emailcontroller = TextEditingController();
+  final _passwordcontroller = TextEditingController();
+
+  List<dynamic> _cities = [];
+  String? _selectedCity;
+  List<String> _universities = [];
+  String? _selectedUniversity;
+
+  @override
+  void initState() {
+    super.initState();
+    loadJsonData();
+  }
+
+  Future<void> loadJsonData() async {
+    final String response = await rootBundle.loadString('Assets/universities.json');
+    final data = await json.decode(response);
+    setState(() {
+      _cities = data['cities'];
+    });
+  }
 
   @override
   void dispose() {
@@ -19,87 +39,151 @@ class _RegisterPageState extends State<RegisterPage> {
     _passwordcontroller.dispose();
     super.dispose();
   }
-  Future signUp() async{
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _emailcontroller.text.trim(), password:_passwordcontroller.text.trim());
+
+  Future signUp() async {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: _emailcontroller.text.trim(),
+      password: _passwordcontroller.text.trim(),
+    );
     Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.grey.shade300,
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         centerTitle: true,
         title: Text("Üyelik Sayfası"),
         backgroundColor: Colors.grey.shade300,
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(padding: EdgeInsets.symmetric(horizontal: 25),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(12)
-            ),
-            child:Padding(padding: EdgeInsets.only(left: 20),
-            child: TextField(
-              controller:_emailcontroller,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "email",
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: TextField(
+                  controller: _emailcontroller,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "email",
+                  ),
+                ),
               ),
             ),
-            ),
           ),
-          ),
-          SizedBox(height: 10,),
-          Padding(padding: EdgeInsets.symmetric(horizontal: 25),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(12)
-            ),
-            child:Padding(padding: EdgeInsets.only(left: 20),
-            child: TextField(
-              controller: _passwordcontroller,
-              obscureText: true,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "şifre",
+          SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: TextField(
+                  controller: _passwordcontroller,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "şifre",
+                  ),
+                ),
               ),
             ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: DropdownButtonFormField<String>(
+              value: _selectedCity,
+              hint: Text("Şehir seçin"),
+              items: _cities.map<DropdownMenuItem<String>>((city) {
+                return DropdownMenuItem<String>(
+                  value: city['name'],
+                  child: Text(city['name']),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedCity = value;
+                  _universities = _cities
+                      .firstWhere((city) => city['name'] == value)['universities']
+                      .cast<String>();
+                  _selectedUniversity = null; // Reset university selection
+                });
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.grey.shade200,
+              ),
             ),
           ),
-          ),
-          SizedBox(height: 10,),
-          GestureDetector(onTap: (){
-            setState(() {
-              signUp();
-            });
-            
-          },
+          SizedBox(height: 10),
+          if (_selectedCity != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: DropdownButtonFormField<String>(
+                value: _selectedUniversity,
+                hint: Text("Üniversite seçin"),
+                items: _universities.map<DropdownMenuItem<String>>((university) {
+                  return DropdownMenuItem<String>(
+                    value: university,
+                    child: Text(university),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedUniversity = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.grey.shade200,
+                ),
+              ),
+            ),
+          SizedBox(height: 10),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                signUp();
+              });
+            },
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Container(
                 padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(color:Colors.brown,
-                borderRadius: BorderRadius.circular(12)
+                decoration: BoxDecoration(
+                  color: Colors.brown,
+                  borderRadius: BorderRadius.circular(12),
                 ),
                 child: Center(
                   child: Text(
                     "Üye OL!",
                     style: TextStyle(
                       fontSize: 20,
-                      fontWeight:FontWeight.bold,
-                      color: Colors.white),
-                      ),
-                ),
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
+                  ),
+                ),
+              ),
             ),
           ),
-          SizedBox(height: 15,),
-          
+          SizedBox(height: 15),
         ],
       ),
     );
