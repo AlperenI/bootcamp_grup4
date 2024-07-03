e4eff1339b10ae70ddf34526e0f4581a17640e42
AlperenI <alperenuniv@gmail.com>
2024-07-03 14:51:28 +0300

    düzenleme

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,5 +1,5 @@
 import 'package:bootcamp_grup4/firebase/firebase_options.dart';
-import 'package:bootcamp_grup4/main_page.dart';
+import 'package:bootcamp_grup4/main/main_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
