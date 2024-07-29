f00ab36f94128a290b6ace7a6cdb90840f1e19cb
USER <bahrikaraduman@outlook.com>
2024-07-29 19:57:44 +0300

    settingspage eklendi

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,6 +1,27 @@
-import 'package:bootcamp_grup4/utils/const.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+import 'package:bootcamp_grup4/utils/const.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Settings Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SettingsPage(),
+    );
+  }
+}
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -10,24 +31,151 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  final user=FirebaseAuth.instance.currentUser!;
+  final user = FirebaseAuth.instance.currentUser!;
+  File? _image;
+
+  get backgroundColor => null;
+
+  Future<void> _pickImage() async {
+    try {
+      final pickedFile =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
+      setState(() {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+        }
+      });
+    } catch (e) {
+      print("Hatalı fotoğraf $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bacgroundColor,
-      body:Center(
-        child:Column(mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        automaticallyImplyLeading:
+            false, // Bu, varsayılan geri düğmesini kaldırır.
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text("Giriş Yapılan hesap ${user.email}"),
-            MaterialButton(onPressed:(){
-              FirebaseAuth.instance.signOut(); 
-            },
-            color: Colors.red,
-            child: Text("Çıkış Yap"),
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
+            Expanded(
+              child: Center(
+                child: Text('Ayarlar'),
+              ),
+            ),
+            SizedBox(width: 48), // Sağ tarafta boşluk bırakmak için
           ],
-        )
+        ),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Center(
+                child: GestureDetector(
+                  onTap: _pickImage,
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: _image != null ? FileImage(_image!) : null,
+                    child: _image == null
+                        ? Icon(
+                            Icons.add_a_photo,
+                            size: 60,
+                            color: Colors.grey[700],
+                          )
+                        : null,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "${user.email}",
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              _buildTextField(
+                label: 'Yeni Şifreniz',
+                isPassword: true,
+              ),
+              const SizedBox(height: 20),
+              _buildTextField(
+                label: 'Şifre Doğrulama',
+                isPassword: true,
+              ),
+              const SizedBox(height: 20),
+              _buildTextField(
+                label: 'Telefon',
+              ),
+              const SizedBox(height: 70),
+              ElevatedButton(
+                onPressed: () {
+                  // kaydet
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 184, 187, 189),
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  'Ayarları Kaydet',
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 184, 187, 189),
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  "Çıkış Yap",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required String label,
+    String? initialValue,
+    bool isPassword = false,
+  }) {
+    return TextField(
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        filled: true,
+        fillColor: Colors.grey[200],
+      ),
+      controller: initialValue != null
+          ? TextEditingController(text: initialValue)
+          : null,
     );
   }
 }
